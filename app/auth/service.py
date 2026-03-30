615c0b410c6c74a9cb85cdf515d9b3d6e38969d9
LipiZed <kometa1810@gmail.com>
2026-03-30 13:19:40 +0300

    second

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,3 +1,63 @@
+from datetime import datetime, timedelta, timezone
+from app.auth.schemas import UserCreate
+from app.core.config import settings
 from passlib.context import CryptContext
+from jose import jwt
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
+from app.models.client import Client
+from fastapi import HTTPException, status
+
 
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+
+def create_access_token(data: dict):
+    expire_time = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {**data, "exp": expire_time}
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return token
+
+
+async def register_user(data: UserCreate, db: AsyncSession):
+    result = await db.execute(select(User).where(User.email == data.email))
+    user = result.scalar_one_or_none()
+    
+    if user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+    
+    new_hashed_password = hash_password(data.password.get_secret_value())
+    new_user = User(email=data.email, hashed_password=new_hashed_password, is_active=True)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    
+    new_client = Client(user_id = new_user.id, full_name = data.full_name, phone = data.phone)
+    db.add(new_client)
+    await db.commit()
+    await db.refresh(new_client)
+    
+    return new_user
+
+
+
+async def authenticate_user(email: str, password: str, db: AsyncSession):
+    result = await db.execute(select(User).where(User.email == email))
+    user = result.scalar_one_or_none()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    if not verify_password(password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    return user
